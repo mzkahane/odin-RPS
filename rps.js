af1e3e142a210a772f04f1850a40c55576800c5b
mzkahane <80354992+mzkahane@users.noreply.github.com>
2025-05-29 21:16:11 -0400

    Add button logic

--- rps.js
+++ rps.js
@@ -12,34 +12,64 @@ function getComputerChoice() {
     return choice
 }
 
-function getHumanChoice() {
-    let choice = prompt("Rock, Paper, or Scissors?")
-    choice = choice.toUpperCase()
-    console.log(choice)
-    return choice
+function getHumanChoie(clicked) {
+    let humanScore = 0
+    let computerScore = 0
+
+    let result = playRound(clicked, getComputerChoice());
+
+    if (result === "HUMAN") {
+        humanScore += 1
+    } else if (result === "COMPUTER") {
+        computerScore += 1
+    }
+
+    return;
 }
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log("DRAW")
+        console.log("---DRAW---")
         return "DRAW"
     } else if (
         (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
         (humanChoice === "PAPER" && computerChoice === "ROCK") ||
         (humanChoice === "SCISSORS" && computerChoice === "PAPER")
     ) {
-        console.log("HUMAN WINS")
+        console.log("---HUMAN WINS---")
         return "HUMAN"
     } else {
-        console.log("COMPUTER WINS")
+        console.log("---COMPUTER WINS---")
         return "COMPUTER"
     }
 
 }
 
 function playGame() {
-    let humanScore = 0
-    let computerScore = 0
+
+    const rockBtn = document.querySelector("#rock");
+    const paperBtn = document.querySelector("#paper");
+    const scissorsBtn = document.querySelector("#scissors");
+
+    let result;
+
+    rockBtn.addEventListener("click", () => {
+        console.log("ROCK");
+        getHumanChoie("ROCK");
+        
+    });
+
+    paperBtn.addEventListener("click", () => {
+        console.log("PAPER");
+        getHumanChoie("PAPER");
+        
+    });
+
+    scissorsBtn.addEventListener("click", () => {
+        console.log("SCISSORS");
+        getHumanChoie("SCISSORS")
+        
+    });
 
     console.log("SCORE H:" + humanScore + " C:" + computerScore)
     return
